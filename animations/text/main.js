f7d712724d1731c5da27732367b14a4250184e78
Sébastien Matos <matos.sebastien@gmail.com>
2025-09-28 13:44:02 +0200

    add text anim

--- animations/text/main.js
+++ animations/text/main.js
@@ -1,1 +1,26 @@
-console.log('yesss');
+import lottie from "lottie-web";
+import { createAnimationApi } from "lottie-api";
+import animationData from "./data.json";
+import "./style.css";
+
+document.title = "Text";
+const container = document.querySelector(".lottie");
+const animation = lottie.loadAnimation({
+  container: container,
+  animationData: animationData,
+  renderer: "svg",
+  loop: false,
+  autoplay: false,
+});
+
+// const animationAPI = api.createAnimationApi(animation);
+
+const button = container.querySelector(".button");
+
+button.onpointerdown = () => {
+  animation.goToAndPlay("press");
+};
+
+button.onpointerup = () => {
+  animation.goToAndPlay("release");
+};
